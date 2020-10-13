5ad46f49deab64a8793c98cd53be57aa0770a623
kerguio <serge@acker.fr>
2020-10-13 21:24:31 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // SET GLOBAL VARIABLES
 
 // URL of CSV file containing geocoded data
-var csvurl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRn02izdqj3IaHV4522anSQ0h098_hp5YoR1a5tXxiaQFrfxU-AQqjnfG5aGccG2qTFXblcOq4fYgHo/pub?output=csv";
+var csvurl = "https://www.dropbox.com/s/omew1pxxxftqi7f/BP%20Data%20-%20Sheet1.csv?dl=0";
 var icourl = "img/marker.png"
 
 // Initialize event listeners for filter checkboxes
